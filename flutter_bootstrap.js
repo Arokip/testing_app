597c5d4517c64347bb6eba8dbbf3b6c8279642c5
Jindřich Pikora <jindrich.pikora@gmail.com>
2025-03-20 18:55:49 +0100

    new web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3975917651"
+    serviceWorkerVersion: "3787703395"
   }
 });
